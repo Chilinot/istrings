ede880b6d957f67f3b49d9ba6ccff2af9cff0227
Lucas Arnström <lucasarnstrom@gmail.com>
2014-10-09 13:47:00 +0200

    foo

--- istring.c
+++ istring.c
@@ -1,10 +1,11 @@
 #include "istring.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 //Define Macros
-#define START(p)  p - sizeof(int)
-#define STRING(p) p + sizeof(int)
+#define START(p) (p - sizeof(int))
+#define STRING(p) (p + sizeof(int))
 
 typedef struct _istring {
     int length;
@@ -29,20 +30,56 @@ char* istring_mk(const char* str) {
     new_istring->length = str_length;
     strcpy(new_istring->string, str);
     
-    return (char*) STRING(new_istring);
+    return new_istring->string;
 }
 
-int foo(char* str) {
-    int* yo = (int*) START(str);
-    return *yo;
+void istring_rm(char* str) {
+    Istring istr = (Istring) START(str);
+    free(istr);
+}
+
+char* istring_to_string(const char* str) {
+    Istring istr = (Istring) START(str);
+    
+    char* new_str = malloc(sizeof(char) * istr->length);
+    if(!new_str) {
+        return NULL;
+    }
+
+    strcpy(new_str, str);
+    
+    return new_str;
+}
+
+size_t istrfixlen(char* str) {
+    int length = 0;
+
+    // This loop will continue until segfault or nonprintable has been found.
+    for(int i = 0 ;; i++) {
+        if(!isprint(str[i])) {
+            str[i] = '\0'; // Place null-char and exit loop
+            break;
+        }
+        ++length;
+    }
+
+    // Retrieve Istring-structure
+    Istring istr = (Istring) START(str);
+    
+    // If length differs, reset it
+    if(length != istr->length) {
+        istr->length = length;
+    }
+    
+    return (size_t) length;
+}
+
+size_t istrlen(const char* str) {
+    Istring istr = (Istring) START(str);
+    return (size_t) istr->length;
 }
 
 int main(void) {
-    char* istr = istring_mk("Tjenare");
-    puts(istr);
-
-    int len = foo(istr);
-    printf("%d\n", len);
-
+    printf("%d\n",(int) istrlen(istring_mk("Tjenare")));
     return 0;
 }
