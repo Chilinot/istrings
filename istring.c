d2acf306775fa7f9f88ba68a99213aa6c4f534b7
Lucas Arnström <lucasarnstrom@gmail.com>
2014-10-19 14:55:53 +0200

    Simplified istrcmp

--- istring.c
+++ istring.c
@@ -137,17 +137,7 @@ int istrcmp(const char *s1, const char *s2) {
         return -1;
     }
     else {
-        while(*s1 != '\0') {
-            if(*s2 == '\0') return 1;
-            if(*s1 < *s2) return -1;
-            if(*s1 > *s2) return 1;
-            s1++;
-            s2++;
-        }
-
-        if(*s2 != '\0') return -1;
-
-        return 0;
+        return istrncmp(s1, s2, s1_len);
     }
 }
 
